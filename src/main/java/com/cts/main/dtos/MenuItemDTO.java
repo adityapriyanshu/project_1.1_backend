9397f6291fecb56cdbd17a813448f848a4669759
Priyanshu <2372542@cognizant.com>
2024-12-21 16:19:50 +0530

    Resolved merge and menu validation

--- src/main/java/com/cts/main/dtos/MenuItemDTO.java
+++ src/main/java/com/cts/main/dtos/MenuItemDTO.java
@@ -1,21 +1,23 @@
 package com.cts.main.dtos;
 
-import org.springframework.format.annotation.NumberFormat;
-import org.springframework.format.annotation.NumberFormat.Style;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class MenuItemDTO {
 
 	@NotBlank(message = "Invlalid foodName: Cannot be blank")
 	@NotNull(message = "Invlalid foodName: Cannot be null")
-	@Size(min=3, max=30, message="Invalid Name: Name should be 3 - 30 characters!")
+	@Size(min = 3, max = 30, message = "Invalid Name: Name should be 3 - 30 characters!")
 	private String foodName;
-	
-	@NotBlank(message = "Invlalid foodPrice: Cannot be blank")
-	@NotNull(message = "Invlalid foodPrice: Cannot be null")
-	@NumberFormat(style = Style.NUMBER)
+
+	@NotNull(message = "Invalid foodPrice: Cannot be null")
+	@Min(value = 1, message = "Please select a value greater than or equal to 1 for food price!")
+	@Max(value = 100, message = "Please select a value less than or equal to 100 for food price!")
 	private double foodPrice;
 }
--- src/main/java/com/cts/main/services/MenuItemService.java
+++ src/main/java/com/cts/main/services/MenuItemService.java
@@ -1,17 +1,12 @@
 package com.cts.main.services;
 
 import java.util.List;
-
+import com.cts.main.dtos.MenuItemDTO;
 import com.cts.main.entities.MenuItem;
 
 public interface MenuItemService {
-	
-	public List<MenuItem> getAllItems();
-	
-	public MenuItem addFoodItem(MenuItem menuItem);
-	
-	public MenuItem updateFoodItemById(Long id, MenuItem menuitem);
-	
-	public void deleteFoodItemById(Long id);
-	
+    MenuItem addFoodItem(MenuItemDTO menuItemDTO);
+    List<MenuItem> getAllItems();
+    MenuItem updateFoodItemById(Long id, MenuItemDTO menuItemDTO);
+    void deleteFoodItemById(Long id);
 }
